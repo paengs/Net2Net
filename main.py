4166febc3176e4dc4e67894f3e8b97e4e6ac8ebf
KyunghyunPaeng <khpaeng@lunit.io>
2016-05-04 00:14:45 +0900

    add baseline experiments

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ Net2Net using Tensorflow
 
   @ Test in MNIST dataset
   1. Train a teacher network
-  2. Resume training in same architecture
-  3. Train a student network (Net2Wider)
+  2. Train a student network (Net2Wider)
     - # of filters in 'conv1' layer [32->128]
+  3. Random pad (Net2Wider baseline)
   4. Train a student network (Net2Deeper)
     - Insert a new layer after 'conv1' layer
+  5. Random initialization (Net2Deeper baseline)
 
   @ Results
-  NOTE: All parameters are fixed.
-  1. validation accuracy: 96.39%
-  2. validation accuracy: 97.39% 
-  3. validation accuracy: 97.85%
-  4. validation accuracy: 97.75%
+  NOTE: All parameters are fixed expect new weights from Net2Net.
+  1. Validation accuracy: 96.39%
+  2. Validation accuracy: 97.39% 
+  3. Validation accuracy: 97.85%
+  4. Validation accuracy: 97.75%
+  5. Validation accuracy: 97.75%
 
 Author: Kyunghyun Paeng
 
@@ -65,13 +67,51 @@ def train_a_student_network_deeper():
             variables_to_restore = tf.get_collection(variables.VARIABLES_TO_RESTORE)
             saver = tf.train.Saver(variables_to_restore)
             saver.restore(sess, WEIGHT)
+            print 'Net2Deeper...'
             for i in range(MAX_ITER):
                 batch = mnist.train.next_batch(BATCH_SIZE)
                 sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
                 if i % 100 == 0:
                     summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
                     writer.add_summary(summary_str, i)
-                    print acc
+                    print '[Iter: {}] Validation Accuracy : {:.4f}'.format(i,acc)
+
+def train_a_student_network_deeper_rand_init():
+    with tf.Graph().as_default():
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+            x = tf.placeholder(tf.float32, shape=[None, 784])
+            y_ = tf.placeholder(tf.float32, shape=[None, 10])
+            x_image = tf.reshape(x, [-1,28,28,1])
+            net = ops.conv2d(x_image, 32, [5, 5], scope='conv1', stddev=0.1, bias=0.1)
+            net = ops.conv2d(net, 32, [5, 5], scope='conv1_new', stddev=0.1, bias=0.1, restore=False)
+            net = ops.max_pool(net, [2, 2], scope='pool1')
+            net = ops.conv2d(net, 64, [5, 5], scope='conv2', stddev=0.1, bias=0.1)
+            net = ops.max_pool(net, [2, 2], scope='pool2')
+            net = ops.flatten(net, scope='pool2_flat')
+            net = ops.fc(net, 1024, scope='fc1', stddev=0.1, bias=0.1)
+            net = ops.fc(net, 10, activation=None, scope='fc2', stddev=0.1, bias=0.1)
+            y_conv = tf.nn.softmax(net)
+            cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+            model = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+            correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.scalar_summary('loss', cross_entropy)
+            tf.scalar_summary('acc', accuracy)
+            merged = tf.merge_all_summaries()
+            saver = tf.train.Saver()
+            writer = tf.train.SummaryWriter('./logs-deeper-rand', sess.graph)
+            sess.run(tf.initialize_all_variables())
+            variables_to_restore = tf.get_collection(variables.VARIABLES_TO_RESTORE)
+            saver = tf.train.Saver(variables_to_restore)
+            saver.restore(sess, WEIGHT)
+            print 'Net2Deeper Baseline (Rand init)...'
+            for i in range(MAX_ITER):
+                batch = mnist.train.next_batch(BATCH_SIZE)
+                sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
+                if i % 100 == 0:
+                    summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+                    writer.add_summary(summary_str, i)
+                    print '[Iter: {}] Validation Accuracy : {:.4f}'.format(i,acc)
 
 def train_a_student_network_wider():
     new_width_conv = 128
@@ -102,14 +142,53 @@ def train_a_student_network_wider():
             variables_to_restore = tf.get_collection(variables.VARIABLES_TO_RESTORE)
             saver = tf.train.Saver(variables_to_restore)
             saver.restore(sess, WEIGHT)
+            print 'Net2Wider...'
             for i in range(MAX_ITER):
                 batch = mnist.train.next_batch(BATCH_SIZE)
                 sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
                 if i % 100 == 0:
                     summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
                     writer.add_summary(summary_str, i)
-                    print acc
+                    print '[Iter: {}] Validation Accuracy : {:.4f}'.format(i,acc)
     
+def train_a_student_network_wider_rand_pad():
+    new_width_conv = 128
+    new_w1, new_b1, new_w2, new_b2 = tf_net2wider_rand(MODEL, WEIGHT, 'conv1', 'conv2', new_width_conv)
+    with tf.Graph().as_default():
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+            x = tf.placeholder(tf.float32, shape=[None, 784])
+            y_ = tf.placeholder(tf.float32, shape=[None, 10])
+            x_image = tf.reshape(x, [-1,28,28,1])
+            net = ops.conv2d(x_image, new_width_conv, [5, 5], scope='conv1', initializer='constant', weights=new_w1, bias=new_b1, restore=False)
+            net = ops.max_pool(net, [2, 2], scope='pool1')
+            net = ops.conv2d(net, 64, [5, 5], scope='conv2', initializer='constant', weights=new_w2, bias=new_b2, restore=False)
+            net = ops.max_pool(net, [2, 2], scope='pool2')
+            net = ops.flatten(net, scope='pool2_flat')
+            net = ops.fc(net, 1024, scope='fc1', stddev=0.1, bias=0.1)
+            net = ops.fc(net, 10, activation=None, scope='fc2', stddev=0.1, bias=0.1)
+            y_conv = tf.nn.softmax(net)
+            cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+            model = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+            correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.scalar_summary('loss', cross_entropy)
+            tf.scalar_summary('acc', accuracy)
+            merged = tf.merge_all_summaries()
+            saver = tf.train.Saver()
+            writer = tf.train.SummaryWriter('./logs-wider-rand', sess.graph)
+            sess.run(tf.initialize_all_variables())
+            variables_to_restore = tf.get_collection(variables.VARIABLES_TO_RESTORE)
+            saver = tf.train.Saver(variables_to_restore)
+            saver.restore(sess, WEIGHT)
+            print 'Net2Wider Baseline (Rand pad)...'
+            for i in range(MAX_ITER):
+                batch = mnist.train.next_batch(BATCH_SIZE)
+                sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
+                if i % 100 == 0:
+                    summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+                    writer.add_summary(summary_str, i)
+                    print '[Iter: {}] Validation Accuracy : {:.4f}'.format(i,acc)
+
 def train_a_teacher_network():
     x = tf.placeholder(tf.float32, shape=[None, 784])
     y_ = tf.placeholder(tf.float32, shape=[None, 10])
@@ -133,48 +212,15 @@ def train_a_teacher_network():
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         writer = tf.train.SummaryWriter('./logs', sess.graph)
         sess.run(tf.initialize_all_variables())
+        print 'Teacher Network...'
         for i in range(MAX_ITER):
             batch = mnist.train.next_batch(BATCH_SIZE)
             sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
             if i % 100 == 0:
                 summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
                 writer.add_summary(summary_str, i)
-                print acc
+                print '[Iter: {}] Validation Accuracy : {:.4f}'.format(i,acc)
                 saver.save(sess, 'my-model', global_step=TEST_ITER)
-
-def resume_train_a_teacher_network():
-    # Rebuild a new graph!
-    with tf.Graph().as_default():
-        x = tf.placeholder(tf.float32, shape=[None, 784])
-        y_ = tf.placeholder(tf.float32, shape=[None, 10])
-        x_image = tf.reshape(x, [-1,28,28,1])
-        net = ops.conv2d(x_image, 32, [5, 5], scope='conv1', stddev=0.1, bias=0.1)
-        net = ops.max_pool(net, [2, 2], scope='pool1')
-        net = ops.conv2d(net, 64, [5, 5], scope='conv2', stddev=0.1, bias=0.1)
-        net = ops.max_pool(net, [2, 2], scope='pool2')
-        net = ops.flatten(net, scope='pool2_flat')
-        net = ops.fc(net, 1024, scope='fc1', stddev=0.1, bias=0.1)
-        net = ops.fc(net, 10, activation=None, scope='fc2', stddev=0.1, bias=0.1)
-        y_conv = tf.nn.softmax(net)
-        cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-        model = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-        correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        tf.scalar_summary('loss', cross_entropy)
-        tf.scalar_summary('acc', accuracy)
-        merged = tf.merge_all_summaries()
-        saver = tf.train.Saver()
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-            writer = tf.train.SummaryWriter('./logs-resume', sess.graph)
-            sess.run(tf.initialize_all_variables())
-            saver.restore(sess, WEIGHT)
-            for i in range(MAX_ITER):
-                batch = mnist.train.next_batch(BATCH_SIZE)
-                sess.run(model, feed_dict={x: batch[0], y_: batch[1]})
-                if i % 100 == 0:
-                    summary_str, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-                    writer.add_summary(summary_str, i)
-                    print acc
 
 def load_teacher_net(sess, model, weights):
     saver = tf.train.import_meta_graph(model)
@@ -202,6 +248,15 @@ def tf_net2wider(model, weight, target_layer, next_layer, new_width):
         nw1, nb1, nw2 = n2n.wider(w1, b1, w2, new_width, True)
     return nw1, nb1, nw2, b2
 
+def tf_net2wider_rand(model, weight, target_layer, next_layer, new_width):
+    n2n = Net2Net()
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        net = load_teacher_net(sess, model, weight)
+        w1, b1 = get_weight_bias_of_layer(net, target_layer)
+        w2, b2 = get_weight_bias_of_layer(net, next_layer)
+        nw1, nb1, nw2 = n2n.wider_rand(w1, b1, w2, new_width)
+    return nw1, nb1, nw2, b2
+
 def tf_net2deeper(model, weight, target_layer):
     n2n = Net2Net()
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -213,9 +268,11 @@ def tf_net2deeper(model, weight, target_layer):
 if __name__ == '__main__':
     # 1. Train a teacher network
     train_a_teacher_network()
-    # 2. Resume training in same arch.
-    resume_train_a_teacher_network()
-    # 3. Train a student network (Net2Wider)
+    # 2. Train a student network (Net2Wider)
     train_a_student_network_wider()
+    # 3. Random pad (Net2Wider baseline)
+    train_a_student_network_wider_rand_pad()
     # 4. Train a student network (Net2Deeper)
     train_a_student_network_deeper()
+    # 5. Random initialization (Net2Deeper baseline)
+    train_a_student_network_deeper_rand_init()
